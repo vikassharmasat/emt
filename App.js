f32f043fb586777ffa53547679c723ea05184713
vikassharmasat <73014787+vikassharmasat@users.noreply.github.com>
2023-03-14 12:50:36 +0530

    navigation page transition

--- App.js
+++ App.js
@@ -1,29 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { CardStyleInterpolators, createStackNavigator } from '@react-navigation/stack';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import {EmployeeListItemDetail} from "./app/screens/employee-list/employee-list-item-detail/employee-list-item-detail";
+import {
+    EmployeeListItemDetail
+} from "./app/screens/employee-list/employee-list-item-detail/employee-list-item-detail";
 import { EmployeeList } from './app/screens/employee-list/employee-list';
 
 const BottomTab = createBottomTabNavigator();
-const Tab = createNativeStackNavigator();
+const Stack = createStackNavigator();
 
 const EmployeeStack = () => {
     return (
-        <Tab.Navigator initialRouteName={EmployeeList} >
-            <Tab.Screen name="Home" component={EmployeeList} />
-            <Tab.Screen name="employee-detail"  component={EmployeeListItemDetail} />
-        </Tab.Navigator>
+        <Stack.Navigator initialRouteName={ EmployeeList }>
+            <Stack.Screen name="Home"
+                          component={ EmployeeList }
+                          options={ {
+                              cardStyleInterpolator:
+                              CardStyleInterpolators.forHorizontalIOS,
+                          } }/>
+            <Stack.Screen name="employee-detail" component={ EmployeeListItemDetail }
+                          options={ {
+                              cardStyleInterpolator:
+                              CardStyleInterpolators.forHorizontalIOS,
+                          } }/>
+        </Stack.Navigator>
     )
 }
 
 export default function App() {
     return (
         <NavigationContainer>
-            <BottomTab.Navigator initialRouteName={EmployeeList} screenOptions={{headerShown:false}}>
-                <BottomTab.Screen name='Home' component={ EmployeeStack } options={ {
+            <BottomTab.Navigator
+                initialRouteName={ EmployeeList }
+                screenOptions={ { headerShown: false } }
+            >
+                <BottomTab.Screen name='Home Screen' component={ EmployeeStack } options={ {
                     tabBarLabel: 'Home',
                     tabBarIcon: ( { color, size } ) => (
                         <MaterialCommunityIcons name="home" color={ color } size={ size }/>
